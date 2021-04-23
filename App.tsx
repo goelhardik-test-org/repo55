5c4f4705ed0d1898c29341ce3359f98b26db4036
Hardik Goel <hardik.iitr@gmail.com>
2021-04-23 08:41:30 -0700

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import CustomButton from './CustomButton';
+
+function WarningButton() {
+  // return React.createElement(CustomButton, {color: 'red'}, null);
+  return <CustomButton color="red" />;
+}
+import { AbstractKeyword } from "typescript";
+<div>
+<img alt="SDF" src="SDFS" />
+<img src="SDFS" />
+<img src="SDFS" />
+<img src="SDFS" />
+</div>
+
+var nav = (
+    <ul id="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Clients</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+);
+
+<MyButton color="blue" shadowSize={2}>
+  Click Me
+</MyButton>
